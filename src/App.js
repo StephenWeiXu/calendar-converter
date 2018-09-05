69a10d879c92d9a7ab566ade15f4b89e950c1d19
Wei Xu <stephenweixu@gmail.com>
2018-09-04 23:27:34 -0400

    get scroll to highligh tnav to work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Route, NavLink, HashRouter, BrowserRouter, Switch } from "react-router-dom";
-import Demo from "./Demo";
 import ScrollableNav from "./ScrollableNav";
 
 class App extends Component {
@@ -12,8 +11,10 @@ class App extends Component {
 
   render() {
     let navStyle = {
-      "position": "fixed"
+      "position": "fixed",
+      "listStyle": "none",
     }
+
     let sectionStyle = {
       "height": "400px"
     }
@@ -25,24 +26,20 @@ class App extends Component {
     return(
       <HashRouter>
         <div>
-          <ScrollableNav style={navStyle}>
-              <li><NavLink to="/"><span>Home</span></NavLink></li>
-              <li><NavLink to="/section_1"><span>Section 1</span></NavLink></li>
-              <li><NavLink to="/section_2"><span>Section 2</span></NavLink></li>
-              <li><NavLink to="/section_3"><span>Section 3</span></NavLink></li>
-          </ScrollableNav>
+          <div className="nav">
+            <ScrollableNav>
+                <li><NavLink to="/"><span>Home</span></NavLink></li>
+                <li><NavLink to="/section_1"><span>Section 1</span></NavLink></li>
+                <li><NavLink to="/section_2"><span>Section 2</span></NavLink></li>
+                <li><NavLink to="/section_3"><span>Section 3</span></NavLink></li>
+            </ScrollableNav>
+          </div>
 
-          <div>
-            <Route exact path="/" component={Demo} />
-            <Route path="/section_1" component={Demo} />
-            <Route path="/section_2" component={Demo} />
-            <Route path="/section_3" component={Demo} />
-
-            <div>
-              <div id="section_1" style={sectionStyle}><h2 style={h2Style}>Section 1</h2></div>
-              <div id="section_2" style={sectionStyle}><h2 style={h2Style}>Section 2</h2></div>
-              <div id="section_3" style={sectionStyle}><h2 style={h2Style}>Section 3</h2></div>
-            </div>
+          <div className="content">
+              <div id="header"><span>Welcome!</span></div>
+              <div id="section_1"><span>Section 1</span></div>
+              <div id="section_2"><span>Section 2</span></div>
+              <div id="section_3"><span>Section 3</span></div>
           </div>
         </div>
       </HashRouter>
--- src/ScrollableNav.js
+++ src/ScrollableNav.js
@@ -22,19 +22,19 @@ class ScrollableNav extends Component {
     //  }
     // });
 
-    function easeInOutQuad(current_time, start, change, duration) {
-      current_time /= duration/2;
-      if (current_time < 1) return change/2*current_time*current_time + start;
-      current_time--;
-      return -change/2 * (current_time*(current_time-2) - 1) + start;
-    };
-
     function scrollTo(start, to, duration) {
       let change = to - start,
           currentTime = 0,
           increment = 30;
-          
-      let animateScroll = function(){        
+      
+      let easeInOutQuad = (current_time, start, change, duration) => {
+          current_time /= duration/2;
+          if (current_time < 1) return change/2*current_time*current_time + start;
+          current_time--;
+          return -change/2 * (current_time*(current_time-2) - 1) + start;
+      };
+   
+      let animateScroll = () => {        
           currentTime += increment;
           let val = easeInOutQuad(currentTime, start, change, duration);
           window.scrollTo(0, val);
@@ -50,16 +50,38 @@ class ScrollableNav extends Component {
       let sectionID = event.target.location.hash.replace("#/", "");
       
       if(sectionID) {
-        scrollTo(window.pageYOffset, document.getElementById(sectionID).offsetTop, 1200);
+        scrollTo(window.pageYOffset, document.getElementById(sectionID).offsetTop - document.getElementsByClassName("nav")[0].scrollHeight, 1200);
       } else {
         scrollTo(window.pageYOffset, 0, 1200);
       }
     }
+
+    // window.onscroll = (event) => {
+    //   let sectionID = event.target.location.hash.replace("#/", "");
+    //   if(sectionID) {
+    //     let fromTop = document.getElementById(sectionID).offsetTop + document.getElementsByClassName("nav")[0];
+    //     let cur = scrollItems.map(function(){
+    //        if (document.getElementById(sectionID).offsetTop < fromTop)
+    //          return document.getElementById(sectionID);
+    //      });
+
+    //     cur = cur[cur.length-1];
+    //     let id = cur && cur.length ? cur[0].id : "";
+
+    //     if (lastId !== id) {
+    //        lastId = id;
+    //        menuItems
+    //          .parent().removeClass("active")
+    //          .end().filter("[href='#"+id+"']").parent().addClass("active");
+    //     } 
+    //   }
+    // }
+
   }
 
 	render() {
 		return(
-      <ul style={this.props.style}>
+      <ul style={this.props.style} className="header">
         { this.props.children }
       </ul>
     );
