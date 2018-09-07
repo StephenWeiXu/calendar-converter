2c87ebb038aab299d3013d45997bb25a2df85519
Wei Xu <stephenweixu@gmail.com>
2018-09-07 17:20:46 -0400

    test non-hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Route, NavLink, HashRouter, Switch } from "react-router-dom";
-import ScrollableNav from "react-scrollnav";
+import ScrollspyNav from "react-scrollspy-nav";
 
 class App extends Component {
   constructor(props) {
@@ -11,15 +11,34 @@ class App extends Component {
 
   render() {
     return(
+        <div>
+          <div className="nav">
+            <ScrollspyNav scrollSectionIds={["section_1", "section_2", "section_3"]} activeLinkClassName="is-active">
+                <li><a href="/"><span>Home</span></a></li>
+                <li><a href="#section_1"><span>Section 1</span></a></li>
+                <li><a href="#section_2"><span>Section 2</span></a></li>
+                <li><a href="#section_3"><span>Section 3</span></a></li>
+            </ScrollspyNav>
+          </div>
+
+          <div className="content">
+              <div id="header"><span>Welcome!</span></div>
+              <div id="section_1"><span>Section 1</span></div>
+              <div id="section_2"><span>Section 2</span></div>
+              <div id="section_3"><span>Section 3</span></div>
+          </div>
+        </div>
+
+      /*
       <HashRouter>
         <div>
           <div className="nav">
-            <ScrollableNav scrollSectionIds={["section_1", "section_2", "section_3"]}>
+            <ScrollspyNav scrollSectionIds={["section_1", "section_2", "section_3"]} router="HashRouter">
                 <li><NavLink to="/"><span>Home</span></NavLink></li>
-                <li><NavLink to="/section_1"><span>Section 1</span></NavLink></li>
-                <li><NavLink to="/section_2"><span>Section 2</span></NavLink></li>
-                <li><NavLink to="/section_3"><span>Section 3</span></NavLink></li>
-            </ScrollableNav>
+                <li><NavLink to="#section_1"><span>Section 1</span></NavLink></li>
+                <li><NavLink to="#section_2"><span>Section 2</span></NavLink></li>
+                <li><NavLink to="#section_3"><span>Section 3</span></NavLink></li>
+            </ScrollspyNav>
           </div>
 
           <div className="content">
@@ -30,8 +49,31 @@ class App extends Component {
           </div>
         </div>
       </HashRouter>
+      */
     );
   }
 }
 
 export default App;
+
+/*
+<HashRouter>
+  <div>
+    <div className="nav">
+      <ScrollspyNav scrollSectionIds={["section_1", "section_2", "section_3"]}>
+          <li><NavLink to="/"><span>Home</span></NavLink></li>
+          <li><NavLink to="#section_1"><span>Section 1</span></NavLink></li>
+          <li><NavLink to="#section_2"><span>Section 2</span></NavLink></li>
+          <li><NavLink to="#section_3"><span>Section 3</span></NavLink></li>
+      </ScrollspyNav>
+    </div>
+
+    <div className="content">
+        <div id="header"><span>Welcome!</span></div>
+        <div id="section_1"><span>Section 1</span></div>
+        <div id="section_2"><span>Section 2</span></div>
+        <div id="section_3"><span>Section 3</span></div>
+    </div>
+  </div>
+</HashRouter> 
+*/
