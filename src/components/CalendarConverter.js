494fbf6b6a2cc3abf980952f9112babce9202787
Wei Xu <stephenweixu@gmail.com>
2019-12-15 01:01:05 -0500

    integrate lunar solar year conversion script

--- src/components/CalendarConverter.js
+++ src/components/CalendarConverter.js
@@ -1,6 +1,26 @@
 import React, { Component } from "react";
+import {Solar, Lunar, LunarSolarConverter} from "../lib/LunarSolarConverter";
 
 class CalendarConverter extends Component {
+	constructor(props) {
+		super(props);
+
+		this.solar = new Solar();
+		this.lunar = new Lunar();
+		this.converter = new LunarSolarConverter();
+
+		this.solarToLunar();
+	}
+
+	solarToLunar() {
+		this.solar.solarYear = 2019;
+		this.solar.solarMonth = 12;
+		this.solar.solarDay = 15;
+		let lunar = this.converter.SolarToLunar(this.solar);
+		console.log(lunar);
+		return lunar;
+	}
+
 	render() {
 		return (
 			<div className="section-content">
@@ -9,6 +29,7 @@ class CalendarConverter extends Component {
 				It also acconuts for the factor that a React app may use different React router and therefore has 
 				different url patterns (for example the hash pathname in <i>HashRouter</i>).
 				</p>
+				<p>{this.solarToLunar().lunarYear}, {this.solarToLunar().lunarMonth}, {this.solarToLunar().lunarDay}</p>
 			</div>
 		);
 	}
